d4d6a17ce36dd21cddbee9bdfb90c2276df71a2a
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-10-27 04:09:11 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
-#!/bin/bash -xv
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2023 Ryusei Matsuki
+# SPDX-License-Identifier: BSD-3-clause
+ng () {
+	echo NG at Line $1
+	res=1
+}
 
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng $LINENO
 
+[ "$res" = 0 ] && echo OK
+exit $res
