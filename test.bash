58cdc877927892d5c1e32126141aa30ba6a5cdfa
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 04:26:17 +0900

    Add the command

--- test.bash
+++ test.bash
@@ -3,24 +3,25 @@
 # SPDX-License-Identifier: BSD-3-clause
 
 ng () {
-	echo NG at Line $1
-	res=1
+    echo NG at Line $1
+    res=1
 }
 
 res=0
 
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
+out=$(seq 5 | python3 plus)
 [ "${out}" = 15 ] || ng $LINENO
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
+out=$(echo あ | python3 plus)
 [ "$?" = 1 ]      || ng $LINENO
 [ "${out}" = "" ] || ng $LINENO
 
-out=$(echo | ./plus)
+out=$(echo | python3 plus)
 [ "$?" = 1 ]      || ng $LINENO
 [ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
+
