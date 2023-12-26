89a18cf836efa9c09736a0ae9f9152d7730e08cb
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 05:03:55 +0900

    Update test

--- test.bash
+++ test.bash
@@ -10,15 +10,15 @@ ng () {
 res=0
 
 ### I/O TEST ###
-out=$(seq 5 | python3 your_program.py)
+out=$(seq 5 | ./plus) 
 [ "${out}" = "Total: 15\nCount: 5\nAverage: 3.0" ] || ng $LINENO
 
 ### STRANGE INPUT ###
-out=$(echo あ | python3 your_program.py)
+out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng $LINENO
 [ "${out}" = "Error converting 'あ' to integer\nNG at Line 17\nTotal: 0\nCount: 1\nAverage: 0.0" ] || ng $LINENO
 
-out=$(echo | python3 your_program.py)
+out=$(echo | ./plus)
 [ "$?" = 1 ] || ng $LINENO
 [ "${out}" = "Total: 0\nNG at Line 21\nNG at Line 22\nTest Failed\nCount: 0" ] || ng $LINENO
 
