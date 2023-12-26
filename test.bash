456c5dcca3ffedfdc46a8a1788649dcf97f7536d
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 06:35:54 +0900

    Update test

--- test.bash
+++ test.bash
@@ -11,16 +11,15 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng $LINENO
+[ "${out}" = "Total: 15\nCount: 5\nAverage: 3.0" ] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "Error converting 'あ' to integer\nNG at Line 17\nTotal: 0\nCount: 1\nAverage: 0.0" ] || ng $LINENO
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "Total: 0\nNG at Line 21\nNG at Line 22\nTest Failed\nCount: 0" ] || ng $LINENO
 
-[ "$res" = 0 ] && echo OK
-exit $res
+[ "$res" = 0 ] && echo "OK" || echo "Test Failed"
