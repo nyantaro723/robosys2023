249997500f7e9801bffc725cd6441b0df0a89bfb
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 18:40:56 +0900

    Update test

--- test.bash
+++ test.bash
@@ -13,22 +13,22 @@ out=$(seq 5 | ./plus)
 expected=$'Total: 15\nCount: 5\nAverage: 3.0'
 echo "out: ${out}"
 echo "expected: ${expected}"
-[ "$out" = "$expected" ] || ng $LINENO
+[ "$out" == "$expected" ] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 expected=$'NG at Line 13\nError converting '\''あ'\'' to integer\nTotal: 0\nCount: 1\nAverage: 0.0'
 echo "out: ${out}"
 echo "expected: ${expected}"
-[ "$?" = 1 ] || ng $LINENO
-[ "$out" = "$expected" ] || ng $LINENO
+[ "$?" == 1 ] || ng $LINENO
+[ "$out" == "$expected" ] || ng $LINENO
 
 out=$(echo | ./plus)
 expected=$'NG at Line 18\nTotal: 0\nNG at Line 21\nNG at Line 22\nTest Failed\nCount: 0'
 echo "out: ${out}"
 echo "expected: ${expected}"
-[ "$?" = 1 ] || ng $LINENO
-[ "$out" = "$expected" ] || ng $LINENO
+[ "$?" == 1 ] || ng $LINENO
+[ "$out" == "$expected" ] || ng $LINENO
 
-[ "$res" = 0 ] && echo "OK" || echo "Test Failed"
+[ "$res" == 0 ] && echo "OK" || echo "Test Failed"
 
