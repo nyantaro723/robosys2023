4cf6d90103f7b7c56a654a178bd9a9edbb81e779
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 16:23:32 +0900

    Update test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash  
+#!/bin/bash
 # SPDX-FileCopyrightText: 2023 Ryusei Matsuki
 # SPDX-License-Identifier: BSD-3-clause
 
@@ -11,15 +11,19 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
+echo "Debug: I/O TEST - out=$out"
 [ "${out}" = "Total: 15\nCount: 5\nAverage: 3.0" ] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
+echo "Debug: STRANGE INPUT - out=$out"
 [ "$?" = 1 ] || ng $LINENO
 [ "${out}" = "Error converting 'あ' to integer\nNG at Line 17\nTotal: 0\nCount: 1\nAverage: 0.0" ] || ng $LINENO
 
 out=$(echo | ./plus)
+echo "Debug: EMPTY INPUT - out=$out"
 [ "$?" = 1 ] || ng $LINENO
 [ "${out}" = "Total: 0\nNG at Line 21\nNG at Line 22\nTest Failed\nCount: 0" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo "OK" || echo "Test Failed"
+
