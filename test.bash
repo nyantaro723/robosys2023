62f1a4415b00c99bd073f1f729e49a2bc25d996d
Ryusei Matsuki <ryusei.1210@icloud.com>
2023-12-27 19:05:04 +0900

    Update test

--- test.bash
+++ test.bash
@@ -1,19 +1,21 @@
-#!/bin/bash  
+#!/bin/bash
 # SPDX-FileCopyrightText: 2023 Ryusei Matsuki
 # SPDX-License-Identifier: BSD-3-clause
 ng () {
-	echo NG at Line $1
-	res=1
+    echo NG at Line $1
+    res=1
 }
 
 res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-expected=$'Total: 15\nCount: 5\nAverage: 3.0'
+expected='Total: 15
+Count: 5
+Average: 3.0'
 echo "out: ${out}"
 echo "expected: ${expected}"
-[ "$out" == "$expected" ] || ng $LINENO
+[ "${out}" == "${expected}" ] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
@@ -21,14 +23,14 @@ expected=$'NG at Line 13\nError converting '\''あ'\'' to integer\nTotal: 0\nCou
 echo "out: ${out}"
 echo "expected: ${expected}"
 [ "$?" == 1 ] || ng $LINENO
-[ "$out" == "$expected" ] || ng $LINENO
+[ "${out}" == "${expected}" ] || ng $LINENO
 
 out=$(echo | ./plus)
 expected=$'NG at Line 18\nTotal: 0\nNG at Line 21\nNG at Line 22\nTest Failed\nCount: 0'
 echo "out: ${out}"
 echo "expected: ${expected}"
 [ "$?" == 1 ] || ng $LINENO
-[ "$out" == "$expected" ] || ng $LINENO
+[ "${out}" == "${expected}" ] || ng $LINENO
 
 [ "$res" == 0 ] && echo "OK" || echo "Test Failed"
 
